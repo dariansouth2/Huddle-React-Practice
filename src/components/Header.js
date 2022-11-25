6e0e27d0f9e152c7224f3e4333fcf94886d6a1e6
Darian Southerland <darsouthland@gmail.com>
2022-11-24 18:04:34 -0600

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { StyledHeader } from "./styles/Header.styled"
+import { Container } from './styles/container.styled'
+ 
+export default function Header() {
+  return (
+    <StyledHeader>
+        <Container>
+
+        </Container>
+    </StyledHeader>
+  )
+}
